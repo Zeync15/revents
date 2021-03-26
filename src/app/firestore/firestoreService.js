07ba030b927df9b380951cb62da5fc7858ccbd2b
Chai Ze Yen <zeyenchai@gmail.com>
2021-03-27 06:07:53 +0800

    End of Section 11

--- src/app/firestore/firestoreService.js
+++ src/app/firestore/firestoreService.js
@@ -55,3 +55,15 @@ export function cancelEventToggle(event) {
     isCancelled: !event.isCancelled,
   });
 }
+
+export function setUserProfileData(user) {
+  return db
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      displayName: user.displayName,
+      email: user.email,
+      photo: user.photoURL || null,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+}
